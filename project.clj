c9541ab682ea28fa2490546dd05cc15afd86ec3b
Brandon Adams <emidln@gmail.com>
2016-06-24 09:59:05 -0500

    Fork and build a snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/compojure-api "0.24.5"
+(defproject com.emidln/compojure-api "0.24.6-SNAPSHOT"
   :description "Compojure Api"
   :url "https://github.com/metosin/compojure-api"
   :license {:name "Eclipse Public License"
